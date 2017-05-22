c1c998abc151b3462d09f5fa8682e26dee2199d7
冯会涛 <nihaofht@126.com>
2017-05-22 10:25:20 +0800

    1、修改登录匿名的情况下，登录到login

--- src/index.js
+++ src/index.js
@@ -6,33 +6,39 @@ import './index.html';
 
 import * as models from './models'
 
-try{
+try {
 
-    (async function() {
-      let rt = await models.checkAccount()
-      let opt = {history:browserHistory}
-      if(rt.data.status !== 'success'){
-          console.log('check false',rt)
-           console.log('你没有登录')
-           localStorage.setItem("token", "");
-           localStorage.setItem("user","{}");
-      }else{
-
-      }
-      const app = dva(opt);
-      app.use(createLoading());
-      Object.keys(models).filter(v=>v.endsWith('Model')).map(cls=>{
-          console.log('load ... model:',cls)
-          app.model(models[cls])
-      })
-    console.log(app);
-    app.router(require('./router'));
-    app.start('#root');
+    (async function () {
+        let rt = await models.checkAccount();
+        console.log(rt, '----------------rt-----------------')
+        let opt = { history: browserHistory }
+        if (rt.data.status !== 'success') {
+            console.log('check false', rt)
+            console.log('你没有登录')
+            localStorage.setItem("token", "");
+            localStorage.setItem("user", "{}");
+        } else {
+            if (rt.data.data.item.role === "cryptonym") {
+                (async function () {
+                    browserHistory.push('/login')
+                })();
+                
+            }
+        }
+        const app = dva(opt);
+        app.use(createLoading());
+        Object.keys(models).filter(v => v.endsWith('Model')).map(cls => {
+            console.log('load ... model:', cls)
+            app.model(models[cls])
+        })
+        console.log(app);
+        app.router(require('./router'));
+        app.start('#root');
 
 
 
     })();
 
-}catch(e){
+} catch (e) {
     console.error(e.message);
 }
